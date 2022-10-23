2ab5962e9a9b864fa6475afa575810d1d58165ae
yusuffa01 <80820447+yusuffa01@users.noreply.github.com>
2022-10-23 17:19:26 +0700

    Initial Commit

---
+++ src/screen/Stack.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Detail from "./Detail";
+import Home from "./Home";
+import FontAwesome from "react-native-vector-icons/FontAwesome5";
+
+const Stack = createNativeStackNavigator();
+
+const ScreenStack = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName={"Home1"}
+      screenOptions={{
+        headerShown: true,
+        headerTitleAlign: "center",
+        headerTintColor: "white",
+        headerStyle: {
+          backgroundColor: "#88b454",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Home1"
+        component={Home}
+        options={{
+          title: "HILLING.ID",
+          headerLeft: () => <FontAwesome name="bars" size={24} color="white" />,
+          headerRight: () => (
+            <FontAwesome name="user-alt" size={24} color="white" />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={Detail}
+        options={{
+          title: "HILLING.ID",
+          headerRight: () => (
+            <FontAwesome name="user-alt" size={24} color="white" />
+          ),
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default ScreenStack;
---
+++ src/utils/data.jsx
@@ -0,0 +1,206 @@
+export const MASKAPAI = {
+  GRD: {
+    maskapai_id: "GRD",
+    maskapai_nama: "Garuda Airline",
+    maskapai_logo: "../images/garuda.jpg",
+  },
+  SJW: {
+    maskapai_id: "SJW",
+    maskapai_nama: "Sriwijaya Air",
+    maskapai_logo: "../images/Sriwijaya_air.png",
+  },
+  LIO: {
+    maskapai_id: "LIO",
+    maskapai_nama: "Lion Air",
+    maskapai_logo: "../images/lion_air.png",
+  },
+  MPA: {
+    maskapai_id: "MPA",
+    maskapai_nama: "Merpati Air",
+    maskapai_logo: "../images/merpati.png",
+  },
+  PLA: {
+    maskapai_id: "PLA",
+    maskapai_nama: "Pelita Air",
+    maskapai_logo: "../images/pelita_air.png",
+  },
+  CTL: {
+    maskapai_id: "CTL",
+    maskapai_nama: "Citilink",
+    maskapai_logo: "../images/citilink.png",
+  },
+};
+
+export const BANDARA = {
+  SKH: {
+    bandara_kode: "SKH",
+    bandara_nama: "Soekarno Hatta",
+  },
+  SSQII: {
+    bandara_kode: "SSQII",
+    bandara_nama: "Sultan Syarif Kasim 2",
+  },
+  NGR: {
+    bandara_kode: "NGR",
+    bandara_nama: "Ngurah Rai",
+  },
+  JND: {
+    bandara_kode: "JND",
+    bandara_nama: "Juanda",
+  },
+  MIA: {
+    bandara_kode: "MIA",
+    bandara_nama: "Minangkabau",
+  },
+  KLN: {
+    bandara_kode: "KLN",
+    bandara_nama: "Kualanamu",
+  },
+};
+
+export const JADWAL = [
+  {
+    jadwal_id: "1",
+    bandara_kode_keberangkatan: "SKH",
+    bandara_kode_tujuan: "SSQII",
+    maskapai_id: "GRD",
+  },
+  {
+    jadwal_id: "2",
+    bandara_kode_keberangkatan: "SKH",
+    bandara_kode_tujuan: "NGR",
+    maskapai_id: "LIO",
+  },
+  {
+    jadwal_id: "3",
+    bandara_kode_keberangkatan: "SSQII",
+    bandara_kode_tujuan: "NGR",
+    maskapai_id: "SJW",
+  },
+  {
+    jadwal_id: "4",
+    bandara_kode_keberangkatan: "NGR",
+    bandara_kode_tujuan: "SKH",
+    maskapai_id: "GRD",
+  },
+  {
+    jadwal_id: "5",
+    bandara_kode_keberangkatan: "JND",
+    bandara_kode_tujuan: "MIA",
+    maskapai_id: "CTL",
+  },
+  {
+    jadwal_id: "6",
+    bandara_kode_keberangkatan: "JND",
+    bandara_kode_tujuan: "KLN",
+    maskapai_id: "PLA",
+  },
+  {
+    jadwal_id: "7",
+    bandara_kode_keberangkatan: "MIA",
+    bandara_kode_tujuan: "KLN",
+    maskapai_id: "MPA",
+  },
+  {
+    jadwal_id: "8",
+    bandara_kode_keberangkatan: "NGR",
+    bandara_kode_tujuan: "MIA",
+    maskapai_id: "GRD",
+  },
+  {
+    jadwal_id: "9",
+    bandara_kode_keberangkatan: "SKH",
+    bandara_kode_tujuan: "KLN",
+    maskapai_id: "MPA",
+  },
+  {
+    jadwal_id: "10",
+    bandara_kode_keberangkatan: "SSQII",
+    bandara_kode_tujuan: "MIA",
+    maskapai_id: "SJW",
+  },
+  {
+    jadwal_id: "11",
+    bandara_kode_keberangkatan: "SKH",
+    bandara_kode_tujuan: "MIA",
+    maskapai_id: "MPA",
+  },
+  {
+    jadwal_id: "12",
+    bandara_kode_keberangkatan: "SKH",
+    bandara_kode_tujuan: "SSQII",
+    maskapai_id: "MPA",
+  },
+  {
+    jadwal_id: "13",
+    bandara_kode_keberangkatan: "SKH",
+    bandara_kode_tujuan: "SSQII",
+    maskapai_id: "CTL",
+  },
+  {
+    jadwal_id: "14",
+    bandara_kode_keberangkatan: "MIA",
+    bandara_kode_tujuan: "SKH",
+    maskapai_id: "PLA",
+  },
+  {
+    jadwal_id: "15",
+    bandara_kode_keberangkatan: "KLN",
+    bandara_kode_tujuan: "SKH",
+    maskapai_id: "GRD",
+  },
+  {
+    jadwal_id: "16",
+    bandara_kode_keberangkatan: "KLN",
+    bandara_kode_tujuan: "SKH",
+    maskapai_id: "GRD",
+  },
+  {
+    jadwal_id: "17",
+    bandara_kode_keberangkatan: "KLN",
+    bandara_kode_tujuan: "SKH",
+    maskapai_id: "CTL",
+  },
+  {
+    jadwal_id: "18",
+    bandara_kode_keberangkatan: "KLN",
+    bandara_kode_tujuan: "SKH",
+    maskapai_id: "MPA",
+  },
+  {
+    jadwal_id: "19",
+    bandara_kode_keberangkatan: "KLN",
+    bandara_kode_tujuan: "SKH",
+    maskapai_id: "PLA",
+  },
+  {
+    jadwal_id: "20",
+    bandara_kode_keberangkatan: "KLN",
+    bandara_kode_tujuan: "SKH",
+    maskapai_id: "SJW",
+  },
+  {
+    jadwal_id: "21",
+    bandara_kode_keberangkatan: "SSQII",
+    bandara_kode_tujuan: "MIA",
+    maskapai_id: "CTL",
+  },
+  {
+    jadwal_id: "22",
+    bandara_kode_keberangkatan: "SSQII",
+    bandara_kode_tujuan: "MIA",
+    maskapai_id: "LIO",
+  },
+  {
+    jadwal_id: "23",
+    bandara_kode_keberangkatan: "SSQII",
+    bandara_kode_tujuan: "MIA",
+    maskapai_id: "MPA",
+  },
+  {
+    jadwal_id: "24",
+    bandara_kode_keberangkatan: "SSQII",
+    bandara_kode_tujuan: "MIA",
+    maskapai_id: "GRD",
+  },
+];
